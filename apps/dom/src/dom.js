fb63fb086466596cf4b58675c2f8b63c75bb2ebd
hangyangws <hangyangws@qq.com>
2017-05-01 21:50:06 +0800

    -

--- apps/dom/src/dom.js
+++ apps/dom/src/dom.js
@@ -3,18 +3,15 @@
   'use strict'; // 触发严格模式
 
   var dataToUrl = function(_obj) {
-      var _url = [],
-        i;
-      if (_obj && typeof _obj === 'object') {
-        for (i in _obj) {
-          _url.push(i + '=' + _obj[i]);
-        }
+    var _url = [],
+      i;
+    if (_obj && typeof _obj === 'object') {
+      for (i in _obj) {
+        _url.push(i + '=' + _obj[i]);
       }
-      return _url.join('&');
-    },
-    error = function(_msg) {
-      g.console && g.console.error(_msg);
-    };
+    }
+    return _url.join('&');
+  };
 
   var dom = {
     id: function(_id) {
@@ -29,14 +26,14 @@
         method: 'GET', // 默认GET
         url: '',
         data: null,
-        success: error,
+        success: null,
         async: true, // 是否异步，默认true
-        error: error,
+        error: null,
         headers: null
       }, _opt);
 
       if (_opt.data && typeof _opt.data === 'object') {
-        _opt.type === 'GET' && (_opt.url = _opt.url + '?' + dataToUrl(_opt.data));
+        _opt.method === 'GET' && (_opt.url = _opt.url + '?' + dataToUrl(_opt.data));
         _opt.data = JSON.stringify(_opt.data);
       }
 
@@ -44,20 +41,21 @@
         dataWatch = function() {
           try {
             if (xhrObj.readyState === XMLHttpRequest.DONE) {
-              if ((xhrObj.status > 200 && xhrObj.status < 300) || xhrObj.status == 304) {
-                _opt.success(xhrObj.responseText);
+              if ((xhrObj.status >= 200 && xhrObj.status < 300) || xhrObj.status == 304) {
+                _opt.success && _opt.success(xhrObj.responseText);
               } else {
-                _opt.error(xhrObj.status);
+                _opt.error && _opt.error(xhrObj.status);
               }
             }
           } catch (e) {
-            _opt.error(e.description);
+            _opt.error && _opt.error(e.description);
           }
         };
 
+      xhrObj.open(_opt.method, _opt.url, _opt.async);
 
       // 请求头设置
-      if (_opt.type === 'POST') {
+      if (_opt.method === 'POST') {
         // POST传递表单数据
         xhrObj.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         // 接受与发送都是json格式
@@ -72,13 +70,13 @@
       }
 
       xhrObj.onreadystatechange = dataWatch;
-      xhrObj.open(_opt.type, _opt.url, _opt.async);
       xhrObj.send(_opt.data);
-    };
-
-    HTMLElement.prototype.find = HTMLElement.prototype.find || function(_class) {
-      return this.querySelectorAll(_class);
     }
+  }
 
-    g.$ = dom;
-  }(this, document);
+  HTMLElement.prototype.find = HTMLElement.prototype.find || function(_class) {
+    return this.querySelectorAll(_class);
+  };
+
+  g.$ = dom;
+}(this, document);
